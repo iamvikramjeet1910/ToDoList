206fcf0dda0746db28b17dd2a574720961749fa0
iamvikramjeet1910 <chhotu123kota@gmail.com>
2024-08-15 23:48:42 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Vikram Kumar on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
